b03fcaedeb8861ee27a7578ace484e542247913f
yuzhoudehua521 <yuzhoudehua521@gmail.com>
2020-10-08 18:19:29 +0800

    'se'

---
+++ src/array/equal.js
@@ -0,0 +1,15 @@
+/* 
+    判断两个数组是否相等
+*/
+export function equal(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require("path");
 
 module.exports = {
+  //1 模式
   mode: "development",
   //mode: 'production', 打包压缩
+
+  //2 入口
   entry: "./src/index.js",
+
+  //3 出口
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "js-utils.js",
@@ -13,4 +18,32 @@ module.exports = {
     //对外暴露名
     library: "jsU",
   },
+
+  //4 模块加载器
+  module: {
+    rules: [
+      //1 处理es6 => es5
+      // {
+      //   test：/\.js/
+      //   include：[path.resolve(__dirname, "src")]
+      //   use：'babel-loader'
+      // }
+      //2 处理打包图片
+      //3 处理音视频或字体
+      //4 处理样式
+      // {
+      //   test: /\.css$/,
+      //   use:  [{
+      //     loader: "style-loader" // 将 JS 字符串生成为 style 节点
+      //   }, {
+      //       loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+      //   }, {
+      //       loader: "sass-loader" // 将 Sass 编译成 CSS
+      //   }]
+      //  }
+    ],
+  },
+
+  //5 插件
+  plugins: [],
 };
